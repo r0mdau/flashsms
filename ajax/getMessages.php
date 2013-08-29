26b9474de3836fcd08cf90d100525a800595cc66
Romain Dauby <romain.dauby@gmail.com>
2013-08-29 18:34:33 +0200

    Indentation standard

--- ajax/getMessages.php
+++ ajax/getMessages.php
@@ -5,16 +5,16 @@ echo getMessagesHere();
 
 function getMessagesHere(){
     global $db;    
-	$datas = $db->table('messages')->find();
-	$html = ''; $x = count($datas);
-	for($x = $x - 1; $x >= 0; $x--){
-		$message = $datas[$x];
-		if(isset($message->name)){
-			$html.= '<blockquote>';
-			$html .= '<p>'.$message->name.' : '.$message->message.'<i class="pull-right icon-remove" style="cursor:pointer" onclick="deletesms(\''.$message->_rid.'\')"></i></p>';
-			$html .= '<small>'.$message->date.'</small>';
-			$html .= '</blockquote><hr>';
-		}
-	}
+    $datas = $db->table('messages')->find();
+    $html = ''; $x = count($datas);
+    for($x = $x - 1; $x >= 0; $x--){
+	$message = $datas[$x];
+        if(isset($message->name)){
+	    $html.= '<blockquote>';
+	    $html .= '<p>'.$message->name.' : '.$message->message.'<i class="pull-right icon-remove" style="cursor:pointer" onclick="deletesms(\''.$message->_rid.'\')"></i></p>';
+	    $html .= '<small>'.$message->date.'</small>';
+            $html .= '</blockquote><hr>';
+        }
+    }
     return $html;
 }

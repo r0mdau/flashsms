09bee57ed725e4044e7c42d93968a867f4268de8
Romain DAUBY <romain.dauby@orange.com>
2014-10-31 14:57:16 +0100

    Refacto + indentation

--- autoload.php
+++ autoload.php
@@ -1,19 +1,22 @@
 <?php
-    function loadDir($dir){        
-        if ($dh = opendir($dir)) {
-            while (($file = readdir($dh)) !== false) {
-                if(strpos($file, '.php') !== false){
-                    require_once($dir.$file);
-                }else if(is_dir($dir.$file) AND $file != '..' AND $file != '.'){
-                    loadDir($dir.$file.'/');
-                }
+function loadDir($dir)
+{
+    if ($dh = opendir($dir)) {
+        while (($file = readdir($dh)) !== false) {
+            if (strpos($file, '.php') !== false) {
+                require_once($dir . $file);
+            } else if (is_dir($dir . $file) AND $file != '..' AND $file != '.') {
+                loadDir($dir . $file . '/');
             }
-            closedir($dh);
         }
+        closedir($dh);
     }
+}
 
-    function autoload(){
-        require_once(__DIR__.'/settings.php');
-        loadDir(__DIR__.'/core/');
-    }
-    spl_autoload_register('autoload');
+function autoload()
+{
+    require_once(__DIR__ . '/settings.php');
+    loadDir(__DIR__ . '/core/');
+}
+
+spl_autoload_register('autoload');
--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
-include ('autoload.php');
+include('autoload.php');
 
 Bootstrap::init($db, $err, $typehead);
-
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -17,81 +16,87 @@ Bootstrap::init($db, $err, $typehead);
 <body>
 <div class="container">
     <div class="span3">
-	<form method="post" class="form-horizontal">
-	<fieldset>
-	    <legend>Envoyer</legend>
-	    <label>Numero</label>
-	    <input type="text" name="number" placeholder="+33 6 xx xx xx xx" autocomplete="off"
-		   value="<?=(isset($_SESSION['user']) ? $_SESSION['user'] : '')?>" 
-		   data-provide="typeahead"
-		   data-items="4"
-		   data-source='<?=$typehead['send']?>'>
-	    <br>
-	    <label>Message</label>
-	    <textarea rows="3" name="message"><?=(isset($_SESSION['message']) ? $_SESSION['message'] : '')?></textarea>
-	    <br>
-	    <label class="checkbox">
-		    <input type="checkbox" name="flash" <?=(isset($_SESSION['flash']) ? 'checked="checked"' : '')?>> Flash
-	    </label>
-	    <br>
-	    <input class="btn" type="submit" value="Envoyer" data-loading-text="En cours d'envoi..." onclick="$(this).button('loading');">
-	</fieldset>
-	</form>
-	<?=info_sending($err)?>
+        <form method="post" class="form-horizontal">
+            <fieldset>
+                <legend>Envoyer</legend>
+                <label>Numero</label>
+                <input type="text" name="number" placeholder="+33 6 xx xx xx xx" autocomplete="off"
+                       value="<?= (isset($_SESSION['user']) ? $_SESSION['user'] : '') ?>"
+                       data-provide="typeahead"
+                       data-items="4"
+                       data-source='<?= $typehead['send'] ?>'>
+                <br>
+                <label>Message</label>
+                <textarea rows="3"
+                          name="message"><?= (isset($_SESSION['message']) ? $_SESSION['message'] : '') ?></textarea>
+                <br>
+                <label class="checkbox">
+                    <input type="checkbox" name="flash" <?= (isset($_SESSION['flash']) ? 'checked="checked"' : '') ?>>
+                    Flash
+                </label>
+                <br>
+                <input class="btn" type="submit" value="Envoyer" data-loading-text="En cours d'envoi..."
+                       onclick="$(this).button('loading');">
+            </fieldset>
+        </form>
+        <?= info_sending($err) ?>
     </div>
     <div class="span3">
-	<form method="post" class="form-horizontal">
-	<fieldset>
-	    <legend>Annuaire</legend>
-	    <label>Prénom Nom</label>
-	    <input type="text" name="name"	autocomplete="off"
-		    data-provide="typeahead"
-		    data-items="4"
-		data-source='<?=$typehead['directory']?>'>
-	    <label>Numero</label>
-	    <input type="text" name="number" placeholder="+33 6 xx xx xx xx">        
-	    <br><br>
-	    <input class="btn btn-warning" type="submit" value="Enregistrer" data-loading-text="Enregistrement..." onclick="$(this).button('loading');">
-	</fieldset>
-	</form>
-	<?=info_directory($err)?>
+        <form method="post" class="form-horizontal">
+            <fieldset>
+                <legend>Annuaire</legend>
+                <label>Prénom Nom</label>
+                <input type="text" name="name" autocomplete="off"
+                       data-provide="typeahead"
+                       data-items="4"
+                       data-source='<?= $typehead['directory'] ?>'>
+                <label>Numero</label>
+                <input type="text" name="number" placeholder="+33 6 xx xx xx xx">
+                <br><br>
+                <input class="btn btn-warning" type="submit" value="Enregistrer" data-loading-text="Enregistrement..."
+                       onclick="$(this).button('loading');">
+            </fieldset>
+        </form>
+        <?= info_directory($err) ?>
     </div>
     <div class="span3">
-	<form method="post" class="form-horizontal">
-	<fieldset>
-	    <legend>Listes de diffusion</legend>
-	    <label>Nom de la liste</label>
-	    <input type="text" name="list"	autocomplete="off"
-		    data-provide="typeahead"
-		    data-items="4"
-		data-source='<?=$typehead['lists']?>'>
-	    <label>Numero / Nom personne</label>
-	    <input type="text" name="number1" autocomplete="off"
-		    placeholder="+33 6 xx xx xx xx"
-		    data-provide="typeahead"
-		    data-items="4"
-		data-source='<?=$typehead['directory']?>'>
-	    <br><br>
-	    <input id="btnpos" class="btn btn-primary" type="submit" value="Enregistrer" data-loading-text="Enregistrement..." onclick="$(this).button('loading');">
-	    <button id="btnlist" type="button" class="btn btn-inverse">+</button>
-	</fieldset>
-	</form>
-	<?=info_list($err)?>
+        <form method="post" class="form-horizontal">
+            <fieldset>
+                <legend>Listes de diffusion</legend>
+                <label>Nom de la liste</label>
+                <input type="text" name="list" autocomplete="off"
+                       data-provide="typeahead"
+                       data-items="4"
+                       data-source='<?= $typehead['lists'] ?>'>
+                <label>Numero / Nom personne</label>
+                <input type="text" name="number1" autocomplete="off"
+                       placeholder="+33 6 xx xx xx xx"
+                       data-provide="typeahead"
+                       data-items="4"
+                       data-source='<?= $typehead['directory'] ?>'>
+                <br><br>
+                <input id="btnpos" class="btn btn-primary" type="submit" value="Enregistrer"
+                       data-loading-text="Enregistrement..." onclick="$(this).button('loading');">
+                <button id="btnlist" type="button" class="btn btn-inverse">+</button>
+            </fieldset>
+        </form>
+        <?= info_list($err) ?>
     </div>
 </div>
 <div class="container">
     <div class="span9">
-	<div id="messages" class="well well-small">
-	    <h3>Messages reçus</h3><hr>
-	    <?=getMessages()?>
-	</div>
+        <div id="messages" class="well well-small">
+            <h3>Messages reçus</h3>
+            <hr>
+            <?= getMessages() ?>
+        </div>
     </div>
 </div>
 <div class="container">
     <div class="span9">
-	<p class="pull-right">
-	    Flashsms by <a href="https://github.com/r0mdau" title="Github r0mdau" target="_blank">r0mdau</a>
-	</p>
+        <p class="pull-right">
+            Flashsms by <a href="https://github.com/r0mdau" title="Github r0mdau" target="_blank">r0mdau</a>
+        </p>
     </div>
 </div>
 <script src="http://code.jquery.com/jquery-latest.min.js"></script>
@@ -101,31 +106,35 @@ Bootstrap::init($db, $err, $typehead);
 <script src="lib/bootstrap/js/bootstrap-typeahead.min.js"></script>
 <script>
     var numero = 1;
-    $(document).ready(function(){
-	$('#btnlist').click(function(){
-	    numero++;
-	    $('#btnpos').before('<input type="text" name="number'+numero+'" autocomplete="off" placeholder="+33 6 xx xx xx xx" data-provide="typeahead" data-items="4" data-source=\'<?=$typehead['directory']?>\'><br><br>');
-	});
-	getMessages();
+    $(document).ready(function () {
+        $('#btnlist').click(function () {
+            numero++;
+            $('#btnpos').before(
+                '<input type="text" name="number' + numero + '" autocomplete="off" ' +
+                'placeholder="+33 6 xx xx xx xx" data-provide="typeahead" data-items="4" ' +
+                'data-source="<?=$typehead['directory']?>"><br><br>'
+            );
+        });
+        getMessages();
     });
-    
+
     function getMessages() {
-	var intervalID = setInterval(function() {
-		$.ajax({
-		    url	: "ajax/getMessages.php",
-		}).done(function(data){
-		    $('#messages').html('');
-	            $('#messages').append('<h3>Messages reçus</h3><hr>'+data);
-		});
-	}, 10000);
+        var intervalID = setInterval(function () {
+            $.ajax({
+                url: "ajax/getMessages.php"
+            }).done(function (data) {
+                $('#messages').html('').append('<h3>Messages reçus</h3><hr>' + data);
+            });
+        }, 10000);
     }
 
-    function deleteMessage(number){
-	$.ajax({url : "ajax/deleteMessage.php?number="+number})
-	.done(function(data){
-	    $('#messages').after(data);
-	    getMessages();
-	});
+    function deleteMessage(number) {
+        $.ajax({
+            url: "ajax/deleteMessage.php?number=" + number
+        }).done(function (data) {
+            $('#messages').after(data);
+            getMessages();
+        });
     }
 </script>
 </body>

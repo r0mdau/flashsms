3642d68a1d7c253dbfe53288172352adde9fd139
Romain DAUBY <romain.dauby@orange.com>
2014-10-31 15:23:15 +0100

    Super todo refacto done

--- core/class/class.Error.php
+++ core/class/class.Error.php
@@ -1,22 +1,52 @@
 <?php
-    class Error{
-        
-        public function __construct(){
-            $this->info = array();
-            $this->warn = array();
-            $this->listoff = array();
-            $this->numberInError = array();
-            
-            $this->in = null;
-            $this->out = null;
-            $this->listin = null;
-        }
-        
-        public $info;
-        public $warn;
-        public $listoff;
-        public $in;
-        public $out;
-        public $listin;
-        public $numberInError;
+
+class Error
+{
+
+    public function __construct()
+    {
+        $this->info = array();
+        $this->warn = array();
+        $this->listoff = array();
+        $this->numberInError = array();
+
+        $this->in = null;
+        $this->out = null;
+        $this->listin = null;
     }
+
+    public function errorMessages()
+    {
+        if (errorSending()) {
+            if (empty($_POST['number'])) {
+                $this->info['number'] = 'Numéro vide ! ';
+            }
+            if (empty($_POST['message'])) {
+                $this->info['message'] = 'Message vide !';
+            }
+            formSetSessionValues();
+        } else if (errorContact()) {
+            if (empty($_POST['number'])) {
+                $this->warn['number'] = 'Numéro vide ! ';
+            }
+            if (empty($_POST['name'])) {
+                $this->warn['name'] = 'Nom de la personne vide !';
+            }
+        } else if (errorList()) {
+            if (empty($_POST['list'])) {
+                $this->listoff['list'] = 'Nom de la list vide ! ';
+            }
+            if (empty($_POST['number1'])) {
+                $this->listoff['number'] = 'Vous devez renseigner 1 numéro minimum !';
+            }
+        }
+    }
+
+    public $info;
+    public $warn;
+    public $listoff;
+    public $in;
+    public $out;
+    public $listin;
+    public $numberInError;
+}
--- core/function/form.function.php
+++ core/function/form.function.php
@@ -1,65 +1,67 @@
 <?php
-//TODO refactoring
-function form(&$err){
-    if(isset($_POST)) global $_POST; else return;        
+function form(&$err)
+{
+    if (isset($_POST)) {
+        global $_POST;
+    } else {
+        return;
+    }
+
     global $db;
-    if(sending()){
-		$_SESSION = array();
-		if(isset($_POST['flash'])) $flash = ' -flash'; else $flash = '';
-        if($list = getlist($_POST['number'])){
-            foreach($list->numbers as $no){
-				$err->out = Gammu::infiniteSend($_POST['message'], $no, $flash);
-				if($err->out != 0)
-					$err->numberInError[] = getNameOf($no);
+    if (sending()) {
+        $_SESSION = array();
+        if (isset($_POST['flash'])) {
+            $flash = ' -flash';
+        } else {
+            $flash = '';
+        }
+
+        if ($list = getlist($_POST['number'])) {
+            foreach ($list->numbers as $no) {
+                $err->out = Gammu::infiniteSend($_POST['message'], $no, $flash);
+                if ($err->out != 0) {
+                    $err->numberInError[] = getNameOf($no);
+                }
             }
-        }else{
-            $data = $db->table('directory')->find();
-            foreach($data as $user){
-                if(isset($user->name) AND $user->name == $_POST['number'])
+        } else {
+            $users = $db->table('directory')->find();
+            foreach ($users as $user) {
+                if (isset($user->name) AND $user->name == $_POST['number']) {
                     $_POST['number'] = $user->number;
+                }
             }
+
             $_POST['number'] = str_replace(' ', '', $_POST['number']);
-			$err->out = Gammu::send($_POST['message'], $_POST['number'], $flash);
-			if($err->out != 0)
-				formSetSessionValues();
-        }        
-    }else if(saveContact()){
+            $err->out = Gammu::send($_POST['message'], $_POST['number'], $flash);
+            if ($err->out != 0) {
+                formSetSessionValues();
+            }
+        }
+    } else if (saveContact()) {
         $_POST['number'] = str_replace(' ', '', $_POST['number']);
         $err->in = $db->table('directory')->insert($_POST);
-    }else if(saveList()){
-        $tab = array("list"=>$_POST['list']);
+    } else if (saveList()) {
+        $list = array("list" => $_POST['list']);
         $i = 1;
-        while(isset($_POST['number'.$i])){
-            if(!empty($_POST['number'.$i])){
-                if($number = getNumberOfName($_POST['number'.$i]))
-                    $tab['numbers'][] = $number;                    
-                else
-                    $tab['numbers'][] = $_POST['number'.$i];
+        while (isset($_POST['number' . $i])) {
+            if (!empty($_POST['number' . $i])) {
+                if ($number = getNumberOfName($_POST['number' . $i])) {
+                    $list['numbers'][] = $number;
+                } else {
+                    $list['numbers'][] = $_POST['number' . $i];
+                }
             }
             $i++;
         }
-        $err->listin = $db->table('lists')->insert($tab);
-    }else if(errorSending()){
-        if(empty($_POST['number']))
-            $err->info['number'] = 'Numéro vide ! ';
-        if(empty($_POST['message']))
-            $err->info['message'] = 'Message vide !';
-	    formSetSessionValues();
-    }else if(errorContact()){
-        if(empty($_POST['number']))
-            $err->warn['number'] = 'Numéro vide ! ';
-        if(empty($_POST['name']))
-            $err->warn['name'] = 'Nom de la personne vide !';
-    }else if(errorList()){
-        if(empty($_POST['list']))
-            $err->listoff['list'] = 'Nom de la list vide ! ';
-        if(empty($_POST['number1']))
-            $err->listoff['number'] = 'Vous devez renseigner 1 numéro minimum !';
+        $err->listin = $db->table('lists')->insert($list);
+    } else {
+        $err->errorMessages();
     }
 }
 
-function formSetSessionValues(){
+function formSetSessionValues()
+{
     $_SESSION['user'] = getNameOf($_POST['number']);
     $_SESSION['message'] = $_POST['message'];
-    if(isset($_POST['flash'])) $_SESSION['flash'] = true;
+    if (isset($_POST['flash'])) $_SESSION['flash'] = true;
 }
